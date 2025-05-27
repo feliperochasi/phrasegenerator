a7deb8d116b66092138f78d274cadc07170f470c
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-27 08:39:00 -0300

    Start project

---
+++ src/main/java/br/com/feliperochasi/phrasegenerator/PhrasegeneratorApplication.java
@@ -0,0 +1,13 @@
+package br.com.feliperochasi.phrasegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhrasegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhrasegeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/feliperochasi/phrasegenerator/PhrasegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.feliperochasi.phrasegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhrasegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
